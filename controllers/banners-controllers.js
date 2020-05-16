a9f0ea01e5d869c1bbeb5113953037d24272bc98
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-05-17 02:25:27 +0530

    added banners controller

--- controllers/banners-controllers.js
+++ controllers/banners-controllers.js
@@ -2,6 +2,27 @@ const Banner = require('../models/banner');
 const HttpError = require('../models/http-error');
 
 const getAllBanners = async (req, res, next) => {
+    let Banners;
+    try {
+        Banners = await Banner.find();
+    } catch (err) {
+        const error = new HttpError(
+            'Fetching Banners failed, please try again later.',
+            500
+        );
+        return next(error);
+    }
+
+    if (!Banners || Banners.length === 0) {
+        return next(
+            new HttpError('Could not find any Banners at the moment.', 404)
+        );
+    }
+    await res.json({
+        banners: Banners.map(banner =>
+            banner.toObject({getters: true})
+        )
+    });
 };
 const getBanner = async (req, res, next) => {
 };
